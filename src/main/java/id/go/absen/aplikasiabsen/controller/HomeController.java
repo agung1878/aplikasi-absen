aa788ae72ce8cd3528302b358858657b1e2fe6b3
abdul rahman agung <abdulrahmanagung7@gmail.com>
2018-04-20 14:47:45 +0700

    Templating

---
+++ src/main/java/id/go/absen/aplikasiabsen/controller/HomeController.java
@@ -0,0 +1,22 @@
+package id.go.absen.aplikasiabsen.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ *
+ * @author agung
+ */
+@Controller
+public class HomeController {
+    @RequestMapping("/")
+    public String dashboard(){
+        return "dashboard";
+    }
+    
+    @ModelAttribute("pageTitle")
+    public String pageTitle() {
+        return "Dashboard";
+    }
+}
