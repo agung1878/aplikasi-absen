87c4d5c86d0d9c8c7ad1d803e57e94857ccfad6d
abdul rahman agung <abdulrahmanagung7@gmail.com>
2018-04-20 10:57:59 +0700

    first commit

---
+++ src/main/java/id/go/absen/aplikasiabsen/AplikasiAbsenApplication.java
@@ -0,0 +1,12 @@
+package id.go.absen.aplikasiabsen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplikasiAbsenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AplikasiAbsenApplication.class, args);
+	}
+}
